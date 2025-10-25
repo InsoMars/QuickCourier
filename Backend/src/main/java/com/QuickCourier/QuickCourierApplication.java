c0dda202daad0c5c6962cf56ad16100cdc3a3d24
InsoMars <marianalandinezrestrepo@gmail.com>
2025-10-24 21:53:39 -0500

    estructura del proyecto back

---
+++ Backend/src/main/java/com/QuickCourier/QuickCourierApplication.java
@@ -0,0 +1,13 @@
+package com.QuickCourier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickCourierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickCourierApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/QuickCourier/QuickCourierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.QuickCourier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickCourierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
