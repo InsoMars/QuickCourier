84ea197e4412d1e7cee2c63708ce041890909cf6
JuanDProg123 <jndmoreno@gmail.com>
2025-11-08 21:02:06 -0500

    Persistencia con DTOs

--- Backend/src/main/java/co/edu/unbosque/springsecurity/dto/ExtraEnvioDTO.java
+++ Backend/src/main/java/co/edu/unbosque/springsecurity/dto/ExtraEnvioDTO.java
@@ -1,10 +1,13 @@
 package co.edu.unbosque.springsecurity.dto;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
  
 @Getter
 @Setter
-
+@NoArgsConstructor
+@AllArgsConstructor
  
  
 public class ExtraEnvioDTO {
--- Backend/src/main/java/co/edu/unbosque/springsecurity/service/PedidoService.java
+++ Backend/src/main/java/co/edu/unbosque/springsecurity/service/PedidoService.java
@@ -41,214 +41,197 @@ import co.edu.unbosque.springsecurity.service.Strategy.PagoStrategy;
 @Service
 public class PedidoService {
 
-@Autowired
-private TarifaFactory tarifaFactory;
+    @Autowired
+    private TarifaFactory tarifaFactory;
 
-@Autowired
- 
-private ExtraEnvioRepository extraEnvioRepository;
+    @Autowired
+    private ExtraEnvioRepository extraEnvioRepository;
 
-@Autowired
-private ProductoRepository productoRepository;
+    @Autowired
+    private ProductoRepository productoRepository;
 
+    @Autowired
+    private ZonaRepository zonaRepository;
 
-@Autowired
-private ZonaRepository zonaRepository;
+    @Autowired
+    private ClienteRepository clienteRepository;
 
-@Autowired
-private ClienteRepository clienteRepository;
+    @Autowired
+    private DescuentoPrimeraCompra descuentoPrimeraCompra;
 
-@Autowired
-private DescuentoPrimeraCompra descuentoPrimeraCompra;
-@Autowired 
-private ControladorPago controladorPago;
+    @Autowired 
+    private ControladorPago controladorPago;
 
-@Autowired
-private FacturaProductoRepository facturaRepository;
-@Autowired
-private DetalleFacturaRepository detalleFacturaRepository;
-@Autowired
-private DetalleExtraRepository detalleExtraRepository;
+    @Autowired
+    private FacturaProductoRepository facturaRepository;
 
+    @Autowired
+    private DetalleFacturaRepository detalleFacturaRepository;
 
-
+    @Autowired
+    private DetalleExtraRepository detalleExtraRepository;
 
 
+    public CalculoEnvioResponseDTO calcularEnvioCompleto(CalculoEnvioDTO pedido, String username) {
+
+        double pesoTotal = calcularPeso(pedido.getProductos());
+        double subtotal = calcularPrecio(pedido.getProductos());
+
+        Tarifa tarifa = TarifaFactory.calcularTarifaBase(pedido.getCiudad());
+
+        List<String> extras = pedido.getExtras() == null ? List.of() : pedido.getExtras();
+        if (extras.contains("regalo")) tarifa = new ExtraEmpaqueRegalo(tarifa);
+        if (extras.contains("express")) tarifa = new ExtraEntregaExpress(tarifa);
+        if (extras.contains("seguro"))  tarifa = new ExtraEnvioSeguro(tarifa);
+        if (extras.contains("fragil"))  tarifa = new ExtraManejoFragil(tarifa);
+
+        double costoEnvio = tarifa.calcularTarifa(pesoTotal);
+        double totalProductos = subtotal;
+
+        //descuentos
+        GestorDescuentos gestor = new GestorDescuentos();
+        gestor.agregarEstrategia(descuentoPrimeraCompra);
+        gestor.agregarEstrategia(new DescuentoFinDeSemana());
+
+        double envioConDescuento = gestor.aplicarDescuentos(costoEnvio, username);
+
+        ControladorPago controladorPago = new ControladorPago();
+        PagoStrategy medioPago = controladorPago.procesarPago(pedido.getMedioPago());
+        Double ajusteMedioPago = medioPago.realizarPago(totalProductos);
+
+        Double iva = 0.19;
+        Double precioDespuesImpuestos = totalProductos + totalProductos * iva;
+        Double costoTotalPedido = precioDespuesImpuestos + ajusteMedioPago + envioConDescuento;
+
+        Cliente cliente = clienteRepository.findByEmail(username)
+                .orElseThrow(() -> new IllegalArgumentException("Cliente no encontrado"));
+
+        Zona zona = zonaRepository.findByNombreZonaIgnoreCase(pedido.getCiudad())
+                .orElseThrow(() -> new IllegalArgumentException("Zona no encontrada"));
+
+        // Guardar factura 
+        Factura factura = toEntity(pedido, cliente, zona, subtotal, pesoTotal, costoTotalPedido, iva);
+        factura = facturaRepository.save(factura);
+
+        // Guardar productos 
+        for (DetalleFacturaDTO detalleDTO : pedido.getProductos()) {
+            Producto producto = productoRepository.findById(detalleDTO.getIdProducto())
+                    .orElseThrow(() -> new IllegalArgumentException("Producto no encontrado"));
+            DetalleFacturaProducto detalle = toEntity(detalleDTO, factura, producto);
+            detalleFacturaRepository.save(detalle);
+        }
+
+        // Guardar extras 
+        if (pedido.getExtras() != null && !pedido.getExtras().isEmpty()) {
+            for (String codigoExtra : pedido.getExtras()) {
+                Extra extra = extraEnvioRepository.findByCodigoExtraIgnoreCase(codigoExtra)
+                        .orElseThrow(() -> new IllegalArgumentException("Extra no encontrado: " + codigoExtra));
+                DetalleExtra detExtra = toEntity(extra, factura);
+                detalleExtraRepository.save(detExtra);
+            }
+        }
+
+        System.out.println(" Ciudad: " + pedido.getCiudad());
+        System.out.println(" Peso total: " + pesoTotal + " kg");
+        System.out.println(" Precio con IVA: " + precioDespuesImpuestos);
+        System.out.println(" Costo envío: " + costoEnvio);
+        System.out.println(" Envio con descuento: " + envioConDescuento);
+        System.out.println(" ajusteMedioPago (medio de pago): " + ajusteMedioPago);
+        System.out.println(" Total final: " + costoTotalPedido);
+
+        return toDTO(factura, envioConDescuento);
+    }
 
 
-public CalculoEnvioResponseDTO calcularEnvioCompleto(CalculoEnvioDTO pedido, String username) {
-   
-    double pesoTotal = calcularPeso(pedido.getProductos());
-    double subtotal = calcularPrecio(pedido.getProductos());
+    private Factura toEntity(CalculoEnvioDTO pedido, Cliente cliente, Zona zona,
+                             double subtotal, double pesoTotal, double costoTotal, double iva) {
+        Factura factura = new Factura();
+        factura.setCliente(cliente);
+        factura.setZona(zona);
+        factura.setFechaFacProd(LocalDateTime.now());
+        factura.setImpuesto(subtotal * iva);
+        factura.setPeso(pesoTotal);
+        factura.setTotalFacProd(costoTotal);
+        return factura;
+    }
 
-
-    Tarifa tarifa = TarifaFactory.calcularTarifaBase(pedido.getCiudad());
-
-
-    List<String> extras = pedido.getExtras() == null ? List.of() : pedido.getExtras();
-    if (extras.contains("regalo")) tarifa = new ExtraEmpaqueRegalo(tarifa);
-    if (extras.contains("express")) tarifa = new ExtraEntregaExpress(tarifa);
-    if (extras.contains("seguro"))  tarifa = new ExtraEnvioSeguro(tarifa);
-    if (extras.contains("fragil"))  tarifa = new ExtraManejoFragil(tarifa);
-
-
-
-    double costoEnvio = tarifa.calcularTarifa(pesoTotal);
-    double totalProductos = subtotal;
-
-
- 
-    GestorDescuentos gestor = new GestorDescuentos();
-    gestor.agregarEstrategia(descuentoPrimeraCompra);
-    gestor.agregarEstrategia(new DescuentoFinDeSemana());
-
-// aplicación descuento 
-    double envioConDescuento = gestor.aplicarDescuentos(costoEnvio, username);
-
-
-
-    ControladorPago controladorPago= new ControladorPago();
-
-    PagoStrategy medioPago= controladorPago.procesarPago(pedido.getMedioPago());
-
-    Double ajusteMedioPago= medioPago.realizarPago(totalProductos);
-
-    Double iva= 0.19;
-
-    Double precioDespuesImpuestos= totalProductos+ totalProductos*iva;
-
-    Double costoTotalPedido= precioDespuesImpuestos+ajusteMedioPago+envioConDescuento;
-
-
-     // guardar en la base
-    Cliente cliente = clienteRepository.findByEmail(username)
-            .orElseThrow(() -> new IllegalArgumentException("Cliente no encontrado"));
-
-    Zona zona = zonaRepository.findByNombreZonaIgnoreCase(pedido.getCiudad())
-            .orElseThrow(() -> new IllegalArgumentException("Zona no encontrada"));
-
-      // creación de la factura
-
-    Factura factura = new Factura();
-    factura.setCliente(cliente);
-    factura.setZona(zona);
-    factura.setFechaFacProd(LocalDateTime.now());
-    factura.setImpuesto(subtotal * iva);
-    factura.setPeso(pesoTotal);
-    factura.setTotalFacProd(costoTotalPedido);
-
-    factura = facturaRepository.save(factura);
-
-     // guardar los productos seleccionados 
-
-    for (DetalleFacturaDTO detalleDTO : pedido.getProductos()) {
-        Producto producto = productoRepository.findById(detalleDTO.getIdProducto())
-                .orElseThrow(() -> new IllegalArgumentException("Producto no encontrado"));
-
+    private DetalleFacturaProducto toEntity(DetalleFacturaDTO dto, Factura factura, Producto producto) {
         DetalleFacturaProducto detalle = new DetalleFacturaProducto();
         detalle.setFactura(factura);
         detalle.setProducto(producto);
-        detalle.setCantidadProducto(detalleDTO.getCantidadProducto());
-        detalle.setSubtotalProducto(producto.getPrecioUniProd() * detalleDTO.getCantidadProducto());
-
-        detalleFacturaRepository.save(detalle);
+        detalle.setCantidadProducto(dto.getCantidadProducto());
+        detalle.setSubtotalProducto(producto.getPrecioUniProd() * dto.getCantidadProducto());
+        return detalle;
     }
 
-    // guardar los extras seleccionados
+    private DetalleExtra toEntity(Extra extra, Factura factura) {
+        DetalleExtra detExtra = new DetalleExtra();
+        detExtra.setFactura(factura);
+        detExtra.setExtra(extra);
+        detExtra.setSubtototalDetalleExtra(extra.getPrecioExtra());
+        return detExtra;
+    }
 
-    if (pedido.getExtras() != null && !pedido.getExtras().isEmpty()) {
-        for (String codigoExtra : pedido.getExtras()) {
-            Extra extra = extraEnvioRepository.findByCodigoExtraIgnoreCase(codigoExtra)
-                    .orElseThrow(() -> new IllegalArgumentException("Extra no encontrado: " + codigoExtra));
-
-            DetalleExtra detExtra = new DetalleExtra();
-            detExtra.setFactura(factura);
-            detExtra.setExtra(extra);
-            detExtra.setSubtototalDetalleExtra(extra.getPrecioExtra());
-
-            detalleExtraRepository.save(detExtra);
-        }
+    private CalculoEnvioResponseDTO toDTO(Factura factura, double envioConDescuento) {
+        return new CalculoEnvioResponseDTO(
+                factura.getTotalFacProd() - factura.getImpuesto(),
+                envioConDescuento,
+                factura.getPeso(),
+                factura.getTotalFacProd()
+        );
     }
 
 
-    // Logs de depuración
-    System.out.println(" Ciudad: " + pedido.getCiudad());
-    System.out.println(" Peso total: " + pesoTotal + " kg");
-    System.out.println(" Precio con IVA: " + precioDespuesImpuestos);
-    System.out.println(" Costo envío: " + costoEnvio);
-    System.out.println(" Envio con descuento: " + envioConDescuento);
-    System.out.println(" ajusteMedioPago (medio de pago): " + ajusteMedioPago); 
-    System.out.println(" Total final: " + costoTotalPedido);
-
-    return new CalculoEnvioResponseDTO(precioDespuesImpuestos, envioConDescuento, pesoTotal, costoTotalPedido);
-}
-
-
-public Double calcularPeso(List<DetalleFacturaDTO> productos) {
- 
+    public Double calcularPeso(List<DetalleFacturaDTO> productos) {
         double pesoTotal = 0;
- 
         for (DetalleFacturaDTO detalle : productos) {
             Producto prod = productoRepository.findById(detalle.getIdProducto())
                     .orElseThrow(() -> new IllegalArgumentException("Producto no encontrado con ID: " + detalle.getIdProducto()));
- 
             pesoTotal += prod.getPesoProd() * detalle.getCantidadProducto();
         }
- 
-        return  pesoTotal;
+        return pesoTotal;
+    }
 
-      }
-
-
- private double calcularPrecio(List<DetalleFacturaDTO> productos) {
+    private double calcularPrecio(List<DetalleFacturaDTO> productos) {
         double total = 0;
         for (DetalleFacturaDTO detalle : productos) {
             Producto prod = productoRepository.findById(detalle.getIdProducto())
-                    .orElseThrow(() -> new IllegalArgumentException(
-                            "Producto no encontrado con ID: " + detalle.getIdProducto()));
+                    .orElseThrow(() -> new IllegalArgumentException("Producto no encontrado con ID: " + detalle.getIdProducto()));
             total += prod.getPrecioUniProd() * detalle.getCantidadProducto();
         }
         return total;
     }
 
 
+    public String obtenerDescripcionTarifa(String ciudad, Double peso,
+                                           boolean empaqueRegalo, boolean envioExpress,
+                                           boolean envioSeguro, boolean manejoFragil) {
 
-   public String obtenerDescripcionTarifa(String ciudad, Double peso, boolean  empaqueRegalo, boolean envioExpress, boolean envioSeguro , boolean manejoFragil) {
+        Tarifa tarifa = TarifaFactory.calcularTarifaBase(ciudad);
 
-    Tarifa tarifa= TarifaFactory.calcularTarifaBase(ciudad);
+        if (empaqueRegalo) tarifa = new ExtraEmpaqueRegalo(tarifa);
+        if (envioExpress) tarifa = new ExtraEntregaExpress(tarifa);
+        if (envioSeguro) tarifa = new ExtraEnvioSeguro(tarifa);
+        if (manejoFragil) tarifa = new ExtraManejoFragil(tarifa);
 
-    if(empaqueRegalo) tarifa= new ExtraEmpaqueRegalo(tarifa);
-   if(envioExpress) tarifa= new ExtraEntregaExpress(tarifa);
-   if (envioSeguro) tarifa = new ExtraEnvioSeguro(tarifa);
-   if(manejoFragil) tarifa = new ExtraManejoFragil(tarifa);
+        return tarifa.getDescripcion();
+    }
 
-   return tarifa.getDescripcion();
+    public List<ExtraEnvioDTO> obtenerExtrasExistentes() {
+        return extraEnvioRepository.findAll()
+                .stream()
+                .map(extra -> new ExtraEnvioDTO(
+                        extra.getNombreExtra(),
+                        extra.getDescripcionExtra(),
+                        extra.getPrecioExtra(),
+                        extra.getCodigoExtra()))
+                .collect(Collectors.toList());
+    }
 
-}
-
-// mostrar extras y zonas 
-
-public List<ExtraEnvioDTO> obtenerExtrasExistentes() {
-    return extraEnvioRepository.findAll()
-            .stream()
-            .map(extra -> {
-                ExtraEnvioDTO dto = new ExtraEnvioDTO();
-                dto.setNombre(extra.getNombreExtra());
-                dto.setDescripcion(extra.getDescripcionExtra());
-                dto.setPrecio(extra.getPrecioExtra());
-                dto.setCodigo(extra.getCodigoExtra());
-                return dto;
-            })
-            .toList();
-}
-
-
-  public List<ZonaDTO> obtenerZonasExistentes() {
-    return zonaRepository.findAll()
-            .stream()
-            .map(zona -> new ZonaDTO(zona.getNombreZona(), zona.getPrecioZona()))
-            .collect(Collectors.toList());
-}
-
-
+    public List<ZonaDTO> obtenerZonasExistentes() {
+        return zonaRepository.findAll()
+                .stream()
+                .map(zona -> new ZonaDTO(zona.getNombreZona(), zona.getPrecioZona()))
+                .collect(Collectors.toList());
+    }
 }
